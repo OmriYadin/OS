7685e8b07024ade40bd699e77f14938a20995757
OS 046209 <omri.yadin@campus.technion.ac.il>
2023-06-23 13:03:28 +0300

    23/06-13:03

--- HW3/ttftps.cpp
+++ HW3/ttftps.cpp
@@ -9,13 +9,14 @@
 #include <netinet/in.h>
 #include <sys/select.h>
 #include <fstream>
+#include <errno.h>
+
 
 #define BUF_MAX_LEN 516
 #define PARAM_MAX_VAL 65535
 #define ERROR 1
 #define FATAL_ERR -1
 #define NON_FATAL_ERR 0
-#define ERR_MSG_LEN 
 
 using namespace std;
 
@@ -59,7 +60,7 @@ struct pck_error {
 int main(int argc, char** argv){
 	//char buffer[BUF_MAX_LEN] = {0};
 	if(argc != 4){
-		cerr << "TTFTP_ERROR: illegal arguments" << endl;
+		cerr << "TTFTP_ERROR illegal arguments" << endl;
 		return ERROR;
 	}
 	int port_int = atoi(argv[1]);
@@ -67,7 +68,7 @@ int main(int argc, char** argv){
 	int max_resends_int = atoi(argv[3]);
 	if((port_int > PARAM_MAX_VAL) || (port_int <= 0) || (timeout_int > PARAM_MAX_VAL) ||
 			(timeout_int <= 0) || (max_resends_int > PARAM_MAX_VAL) || (max_resends_int <= 0)){
-		cerr << "TTFTP_ERROR: illegal arguments" << endl;
+		cerr << "TTFTP_ERROR illegal arguments" << endl;
 		return ERROR;
 	}
 	unsigned short port = (unsigned short)port_int;
@@ -87,11 +88,12 @@ int main(int argc, char** argv){
 		int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 		cout << sockfd << endl;
 		if (sockfd == -1){
-			perror("TTFTP_ERROR:");
+			perror("TTFTP_ERROR");
 			return ERROR;
 		}
 		if(bind(sockfd, (struct sockaddr*)&ser_addr, sizeof(ser_addr)) == -1){
-			perror("TTFTP_ERROR:");
+			cout << "bind" << endl;
+			perror("TTFTP_ERROR");
 			return ERROR;
 		}
 		udp_connection(sockfd, timeout, max_resends);
@@ -105,14 +107,15 @@ void udp_connection(int sockfd, unsigned short timeout, unsigned short max_resen
 	int num_count = 0;
 	struct sockaddr_in cli_addr;
 	memset(&cli_addr, 0, sizeof(cli_addr));
-	socklen_t cli_addrlen;
+	socklen_t cli_addrlen = sizeof(cli_addr);
 	struct pck_wrq wrq;
 	memset(&wrq, 0, sizeof(wrq));
-	ssize_t packet_size = recvfrom(sockfd, (void*)&wrq, BUF_MAX_LEN, 0, 
+	ssize_t packet_size = recvfrom(sockfd, (void*)&wrq, sizeof(wrq), MSG_TRUNC, 
 			(struct sockaddr*)&cli_addr, &cli_addrlen);
 	wrq.opcode = ntohs(wrq.opcode);
+	cout << "wrq packet size: " << packet_size << endl;
 	if (packet_size <= 0){
-		perror("TTFTP_ERROR:");
+		perror("TTFTP_ERROR");
 		exit(ERROR);
 	}
 	if(wrq.opcode != 2){
@@ -120,21 +123,27 @@ void udp_connection(int sockfd, unsigned short timeout, unsigned short max_resen
 		return;
 	}
 	char* file_name = strtok(wrq.strings, "\0");
-	if(access(file_name, F_OK) < 0){
+	int acc = access(file_name, F_OK);
+	if(!acc){
 		file_exists(sockfd, &cli_addr, cli_addrlen);
+		return;
+	}
+	else if (errno != ENOENT){
+		perror("TTFTP_ERROR");
+		exit(ERROR);
 	}
 	fstream new_file;
 	new_file.open(file_name, ios_base::out);
 	send_ack(sockfd, num_count, &new_file, file_name, &cli_addr, cli_addrlen);
 	struct timeval tout;
-	tout.tv_sec = timeout;
-	tout.tv_usec = 0;
 	fd_set rd_set;
-	FD_ZERO(&rd_set);
-	FD_SET(sockfd, &rd_set);
 	int err_cnt = 0;
 	num_count++;
 	while(true){
+		tout.tv_sec = timeout;
+		tout.tv_usec = 0;
+		FD_ZERO(&rd_set);
+		FD_SET(sockfd, &rd_set);
 		int data_rec =  get_data(sockfd, &new_file, (const struct timeval)tout, &num_count,
 				&rd_set, file_name, max_resends, &err_cnt, &cli_addr, cli_addrlen, timeout);
 		if(data_rec == FATAL_ERR){
@@ -148,9 +157,6 @@ void udp_connection(int sockfd, unsigned short timeout, unsigned short max_resen
 		else if (data_rec < BUF_MAX_LEN){
 			break;
 		}
-		tout.tv_sec = timeout;
-		tout.tv_usec = 0;
-		FD_ZERO(&rd_set);
 	}
 	FD_CLR(sockfd, &rd_set);
 }
@@ -158,12 +164,13 @@ void udp_connection(int sockfd, unsigned short timeout, unsigned short max_resen
 
 void wrq_check(int sockfd, struct sockaddr_in* cli_addr, socklen_t cli_addrlen){
 	struct pck_error err;
+	memset(&err, 0, sizeof(err));
 	err.opcode = htons(5);
 	err.err_code = htons(7);
 	strcpy(err.err_msg, "Unknown user");
 	if(sendto(sockfd, (const void*)&err, sizeof(err), MSG_CONFIRM, 
 			(const struct sockaddr*)cli_addr, cli_addrlen) < 0){
-		perror("TTFTP_ERROR:");
+		perror("TTFTP_ERROR");
 		exit(ERROR);
 	}
 	return;
@@ -172,12 +179,13 @@ void wrq_check(int sockfd, struct sockaddr_in* cli_addr, socklen_t cli_addrlen){
 
 void file_exists(int sockfd, struct sockaddr_in* cli_addr, socklen_t cli_addrlen){
 	struct pck_error err;
+	memset(&err, 0, sizeof(err));
 	err.opcode = htons(5);
 	err.err_code = htons(6);
 	strcpy(err.err_msg, "File already exists");
 	if(sendto(sockfd, (const void*)&err, sizeof(err), MSG_CONFIRM, 
 			(const struct sockaddr*)cli_addr, cli_addrlen) < 0){
-		perror("TTFTP_ERROR:");
+		perror("TTFTP_ERROR");
 		exit(ERROR);
 	}
 	return;
@@ -187,6 +195,7 @@ void file_exists(int sockfd, struct sockaddr_in* cli_addr, socklen_t cli_addrlen
 void send_ack(int sockfd, int ack_num, fstream* file, char* file_name,
 		struct sockaddr_in* cli_addr, socklen_t cli_addrlen){
 	struct pck_ack ack;
+	memset(&ack, 0, sizeof(ack));
 	cout << ack_num << "ACK-B" << endl;
 	ack.opcode = htons(4);
 	ack.bl_num = htons(ack_num);
@@ -195,7 +204,7 @@ void send_ack(int sockfd, int ack_num, fstream* file, char* file_name,
 			(const struct sockaddr*)cli_addr, cli_addrlen) < 0){
 		file->close();
 		remove(file_name);
-		perror("TTFTP_ERROR:");
+		perror("TTFTP_ERROR");
 		exit(ERROR);
 	}
 	return;
@@ -207,6 +216,7 @@ void send_error(int sockfd, unsigned short code, char* msg,
 		fstream* file, char* file_name,
 		struct sockaddr_in* cli_addr, socklen_t cli_addrlen){
 	struct pck_error err;
+	memset(&err, 0, sizeof(err));
 	err.opcode = htons(5);
 	err.err_code = htons(code);
 	strcpy(err.err_msg, msg);
@@ -214,7 +224,7 @@ void send_error(int sockfd, unsigned short code, char* msg,
 			(const struct sockaddr*)cli_addr, cli_addrlen) < 0){
 		file->close();
 		remove(file_name);
-		perror("TTFTP_ERROR:");
+		perror("TTFTP_ERROR");
 		exit(ERROR);
 	}
 	return;
@@ -229,7 +239,7 @@ int get_data(int sockfd, fstream* file, struct timeval tout, int* bl_num,
 		if(tmp < 0){
 			file->close();
 			remove(file_name);
-			perror("TTFTP_ERROR:");
+			perror("TTFTP_ERROR");
 			exit(ERROR);
 		}
 		*err_cnt += 1;
@@ -242,6 +252,7 @@ int get_data(int sockfd, fstream* file, struct timeval tout, int* bl_num,
 		tout.tv_sec = timeout;
 		tout.tv_usec = 0;
 		FD_ZERO(rd_set);
+		FD_SET(sockfd, rd_set);
 		tmp = select(sockfd+1, NULL, rd_set, NULL, &tout);
 		cout << "error count is: " << *err_cnt << endl;
 	}
@@ -250,10 +261,15 @@ int get_data(int sockfd, fstream* file, struct timeval tout, int* bl_num,
 	memset(&data, 0, sizeof(data));
 	struct sockaddr_in tmp_addr;	
 	memset(&tmp_addr, 0, sizeof(tmp_addr));
-	socklen_t tmp_addrlen;
+	socklen_t tmp_addrlen = sizeof(tmp_addr);
 	cout << "We wait for data" << endl;
-	ssize_t bytes_rec = recvfrom(sockfd, (void*)&data, BUF_MAX_LEN, 0, 
+	ssize_t bytes_rec = recvfrom(sockfd, (void*)&data, sizeof(data), MSG_TRUNC, 
 			(struct sockaddr*)&tmp_addr, &tmp_addrlen);
+	if (bytes_rec <= 0){
+		perror("TTFTP_ERROR");
+		exit(ERROR);
+	}
+	cout << "data packet size: " << bytes_rec << endl;
 	cout << "We recieved data" << endl;
 	cout << tmp_addr.sin_port << " " << cli_addr->sin_port << endl;
 	cout << tmp_addr.sin_addr.s_addr << " " << cli_addr->sin_addr.s_addr << endl;
